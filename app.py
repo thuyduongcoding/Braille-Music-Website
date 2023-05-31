74d300056400cc2558f3de7f013cb43357c9a23c
Tran Ngoc Dang Khoa <tndangkhoa218@gmail.com>
2023-05-31 23:33:40 +0700

    meow

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, Response
 import convert
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="build/static", template_folder="build")
 
 @app.route('/convert', methods=["GET", "POST"])
 def upload():
@@ -17,9 +18,26 @@ def upload():
         return 'No image in the request'
     return 'Upload page'
 
-# @app.route('/')
-# def hello():
-#     return render_template('upload.html')
+@app.route('/test', methods=["GET", "POST"])
+def test():
+    if request.method == 'POST':
+        if 'image' in request.files:
+            image = request.files['image']
+            print(image)
+            image_path = './input/image.png'
+            image.save(image_path)
+            
+            # Save the image or perform further processing
+            return send_file('./output/sheet.txt', as_attachment=True)
+        print('No image in the request')
+        return 'No image in the request'
+    print('Upload page')
+    return 'Upload page'
+
+    # Serve React App
+@app.route('/')
+def serve():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
